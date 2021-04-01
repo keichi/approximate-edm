54ec64d6e3674906c730ca0b7618c926865f810c
Keichi Takahashi <keichi.t@me.com>
2021-04-01 11:07:49 +0900

    Fix race conditions

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include <cassert>
 #include <iostream>
 #include <random>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 #include <omp.h>
 
 #include "timer.hpp"
 
-const size_t N = 100000;
-const size_t K = 10;
+const size_t N = 1000000;
+const size_t K = 20;
 const float DELTA = 0.001f;
 
 struct Neighbor {
@@ -26,7 +26,7 @@ struct Neighbor {
 
 struct NeighborList {
     std::vector<Neighbor> nodes;
-    std::set<size_t> ids;
+    std::unordered_set<size_t> ids;
 };
 
 using NNGraph = std::vector<NeighborList>;
@@ -46,10 +46,17 @@ float sigma(const std::vector<float> &u, const std::vector<float> &v)
 
 bool update_nns(NeighborList &nns, float l, size_t u, omp_lock_t *lock)
 {
-    if (l >= nns.nodes.front().distance) return false;
-    if (nns.ids.find(u) != nns.ids.end()) return false;
-
     omp_set_lock(lock);
+
+    if (l >= nns.nodes.front().distance) {
+        omp_unset_lock(lock);
+        return false;
+    }
+
+    if (nns.ids.find(u) != nns.ids.end()) {
+        omp_unset_lock(lock);
+        return false;
+    }
 
     nns.ids.erase(nns.nodes.front().id);
     nns.ids.insert(u);
@@ -71,8 +78,7 @@ void nn_descent(const std::vector<std::vector<float>> &data, NNGraph &nng)
         omp_init_lock(&lock);
     }
 
-    std::vector<std::set<size_t>> old_nns(nng.size()), new_nns(nng.size());
-
+    std::vector<std::unordered_set<size_t>> old_nns(nng.size()), new_nns(nng.size());
 
     Timer ttot;
 
@@ -128,8 +134,14 @@ void nn_descent(const std::vector<std::vector<float>> &data, NNGraph &nng)
 
                     const auto l = sigma(data[u1], data[u2]);
 
-                    if (update_nns(nng[u1], l, u2, &locks[u1])) c++;
-                    if (update_nns(nng[u2], l, u1, &locks[u2])) c++;
+                    if (update_nns(nng[u1], l, u2, &locks[u1])) {
+#pragma omp atomic update
+                        c++;
+                    }
+                    if (update_nns(nng[u2], l, u1, &locks[u2])) {
+#pragma omp atomic update
+                        c++;
+                    }
                 }
 
                 for (const auto u2 : old_nns[v]) {
@@ -137,8 +149,14 @@ void nn_descent(const std::vector<std::vector<float>> &data, NNGraph &nng)
 
                     const auto l = sigma(data[u1], data[u2]);
 
-                    if (update_nns(nng[u1], l, u2, &locks[u1])) c++;
-                    if (update_nns(nng[u2], l, u1, &locks[u2])) c++;
+                    if (update_nns(nng[u1], l, u2, &locks[u1])) {
+#pragma omp atomic update
+                        c++;
+                    }
+                    if (update_nns(nng[u2], l, u1, &locks[u2])) {
+#pragma omp atomic update
+                        c++;
+                    }
                 }
             }
         }
@@ -175,6 +193,7 @@ int main()
     for (size_t i = 0; i < N; i++) {
         data[i].push_back(dist(engine));
         data[i].push_back(dist(engine));
+
         for (size_t j = 0; j < K; j++) {
             size_t id = dist2(engine);
             float distance = std::numeric_limits<float>::max();
@@ -186,19 +205,19 @@ int main()
     nn_descent(data, nng);
 
     // std::cout << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
-    //              "xmlns:xlink=\"http://www.w3.org/1999/xlink\">"
-    //           << std::endl;
+                 // "xmlns:xlink=\"http://www.w3.org/1999/xlink\">"
+              // << std::endl;
 
     // for (size_t i = 0; i < N; i++) {
-    //     std::cout << "<circle cx=\"" << (data[i][0] * 500) << "\" cy=\""
-    //               << (data[i][1] * 500) << "\" r=\"3\" />" << std::endl;
+        // std::cout << "<circle cx=\"" << (data[i][0] * 500) << "\" cy=\""
+                  // << (data[i][1] * 500) << "\" r=\"3\" />" << std::endl;
 
-    //     for (const auto id : nng[i].ids) {
-    //         std::cout << "<line x1=\"" << (data[i][0] * 500) << "\" y1=\""
-    //                   << (data[i][1] * 500) << "\" x2=\"" << (data[id][0] * 500)
-    //                   << "\" y2=\"" << (data[id][1] * 500)
-    //                   << "\" stroke=\"black\" />" << std::endl;
-    //     }
+        // for (const auto id : nng[i].ids) {
+            // std::cout << "<line x1=\"" << (data[i][0] * 500) << "\" y1=\""
+                      // << (data[i][1] * 500) << "\" x2=\"" << (data[id][0] * 500)
+                      // << "\" y2=\"" << (data[id][1] * 500)
+                      // << "\" stroke=\"black\" />" << std::endl;
+        // }
     // }
 
     // std::cout << "</svg>" << std::endl;
