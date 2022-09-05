408fab498f3e50e5f4fe7e056b55d09e076a5605
Keichi Takahashi <hello@keichi.dev>
2022-09-05 19:11:28 +0900

    Add nanoflann and various FAISS indexes

--- approx_edm.cpp
+++ approx_edm.cpp
@@ -14,16 +14,22 @@
 #include <xtensor/xview.hpp>
 
 #include <faiss/IndexFlat.h>
+#include <faiss/IndexLSH.h>
+#include <faiss/IndexNSG.h>
+#include <faiss/IndexIVFFlat.h>
+#include <faiss/IndexIVFPQ.h>
 #include <faiss/IndexHNSW.h>
 #include <faiss/IndexNNDescent.h>
+#include <faiss/IndexScalarQuantizer.h>
 #include <faiss/gpu/GpuIndexFlat.h>
 #include <faiss/gpu/StandardGpuResources.h>
+#include <nanoflann.hpp>
 
 #include "timer.hpp"
 
-const int N = 100000;
+const int N = 1000000;
 const float DT = 40.0f / N;
-const int E = 3;
+const int E = 5;
 const int tau = 1;
 const int Tp = 1;
 
@@ -75,6 +81,23 @@ void generate_dataset(xt::xtensor<float, 1> &ts)
     }
 }
 
+template <class T> class XtensorDataset
+{
+    const xt::xtensor<T, 2> &dataset;
+
+public:
+    XtensorDataset(const xt::xtensor<T, 2> &dataset) : dataset(dataset) {}
+
+    inline size_t kdtree_get_point_count() const { return dataset.shape(0); }
+
+    inline T kdtree_get_pt(const size_t idx, int dim) const
+    {
+        return dataset(idx, dim);
+    }
+
+    template <class BBOX> bool kdtree_get_bbox(BBOX &bb) const { return false; }
+};
+
 void simplex(const xt::xtensor<float, 1> &train,
              const xt::xtensor<float, 1> &test)
 {
@@ -95,25 +118,96 @@ void simplex(const xt::xtensor<float, 1> &train,
         xt::empty<float>({test_embed.shape(0), static_cast<size_t>(E + 1)});
     xt::xtensor<faiss::Index::idx_t, 2> ind = xt::empty<faiss::Index::idx_t>(
         {test_embed.shape(0), static_cast<size_t>(E + 1)});
+    // xt::xtensor<unsigned int, 2> ind = xt::empty<faiss::Index::idx_t>(
+        // {test_embed.shape(0), static_cast<size_t>(E + 1)});
 
     faiss::gpu::StandardGpuResources res; // use a single GPU
 
-    Timer timer;
+    Timer timer_train, timer_index, timer_search, timer_total;
 
-    timer.start();
-
+    // Brute-force on CPU -> 462823 ms
     // faiss::IndexFlatL2 index(E);
-    // faiss::IndexHNSWFlat index(E, 4);
-    // faiss::IndexPQ index(E, 1, 8);
-    // faiss::IndexNNDescentFlat index(E, E+1);
+
+    // Brute-force on GPU -> 7297.32 ms
     faiss::gpu::GpuIndexFlatL2 index(&res, E);
-    // index.train(train_embed.shape(0) - Tp, train_embed.data());
+
+    // Inverted file on CPU -> 4000.96 ms
+    // faiss::IndexFlatL2 quantizer(E);
+    // faiss::IndexIVFFlat index(&quantizer, E, 256);
+
+    // HNSW on CPU -> 801.73 ms
+    // faiss::IndexHNSWFlat index(E, 4);
+    // index.hnsw.efConstruction = 2;
+    // index.hnsw.efSearch = 2;
+
+    // NSG on CPU -> ???
+    // faiss::IndexNSGFlat index(E, 32);
+
+    // Inverted file + HNSQ on CPU -> 8719.06 ms
+    // faiss::IndexHNSWFlat quantizer(E, 4);
+    // quantizer.hnsw.efConstruction = 2;
+    // quantizer.hnsw.efSearch = 2;
+    // faiss::IndexIVFFlat index(&quantizer, E, 256);
+
+    // PQ on CPU -> ???
+    // faiss::IndexPQ index(E, 1, 8);
+
+    // Inverted file + PQ on CPU -> 2216.23 ms
+    // faiss::IndexFlatL2 quantizer(E);
+    // faiss::IndexIVFPQ index(&quantizer, E, 256, 1, 4);
+
+    // NN-Descent on CPU -> 73088.4 ms, R2: 0.291908
+    // faiss::IndexNNDescentFlat index(E, E+1);
+
+    // LSH -> incorrect result
+    // faiss::IndexLSH index(E, 8);
+
+    timer_total.start();
+
+    timer_train.start();
+    index.train(train_embed.shape(0), train_embed.data());
+    timer_train.stop();
+    std::cout << "Train index: " << timer_train.elapsed() << " [ms]" << std::endl;
+
+    timer_index.start();
     index.add(train_embed.shape(0) - Tp, train_embed.data());
+    timer_index.stop();
+    std::cout << "Build index: " << timer_index.elapsed() << " [ms]" << std::endl;
+
+    timer_search.start();
     index.search(test_embed.shape(0), test_embed.data(), E + 1, dist.data(),
                  ind.data());
+    timer_search.stop();
+    timer_total.stop();
+    std::cout << "Search kNN: " << timer_search.elapsed() << " [ms]" << std::endl;
 
-    timer.stop();
-    std::cout << "kNN search: " << timer.elapsed() << "[ms]" << std::endl;
+    // timer_total.start();
+    // timer_index.start();
+
+    // k-d tree on CPU -> 13417.4 ms
+    // XtensorDataset<float> dataset(train_embed);
+    // nanoflann::KDTreeSingleIndexAdaptor<
+        // nanoflann::L2_Simple_Adaptor<float, XtensorDataset<float>>,
+        // XtensorDataset<float>>
+        // index(E, dataset);
+    // index.buildIndex();
+    // timer_index.stop();
+    // std::cout << "Build index: " << timer_index.elapsed() << " [ms]" << std::endl;
+
+    // timer_search.start();
+    // #pragma omp parallel for
+    // for (int i = 0; i < test_embed.shape(0); i++) {
+        // index.knnSearch(xt::row(test_embed, i).data() + xt::row(test_embed, i).data_offset(),
+                        // E+1,
+                        // xt::row(ind, i).data() + xt::row(ind, i).data_offset(),
+                        // xt::row(dist, i).data() + xt::row(dist, i).data_offset());
+    // }
+
+    // timer_search.stop();
+    // std::cout << "Search kNN: " << timer_search.elapsed() << " [ms]" << std::endl;
+    // timer_total.stop();
+
+    std::cout << "Total: " << timer_total.elapsed() << " [ms]" << std::endl;
 
     // // Calculate pairwise distance matrix
     // xt::xtensor<float, 2> dmatrix =
@@ -130,6 +224,8 @@ void simplex(const xt::xtensor<float, 1> &train,
     //     xt::view(xt::argsort(dmatrix, 1), xt::all(), xt::range(0, E + 1));
     // auto dist = xt::sqrt(
     //     xt::view(xt::sort(dmatrix, 1), xt::all(), xt::range(0, E + 1)));
+
+    // dist = xt::broadcast(0.1f, dist.shape());
 
     // Calculate weights from distances
     auto min_dist = xt::amin(dist, 1);
@@ -148,10 +244,13 @@ void simplex(const xt::xtensor<float, 1> &train,
     auto actual = xt::view(test, xt::range((E - 1) * tau + Tp, test.size()));
     auto predicted = xt::view(pred, xt::range(0, pred.size() - 1));
 
+    float actual_mean = xt::mean(actual)(0);
     float r2 = 1.0f - xt::sum(xt::square(actual - predicted))(0) /
-                          xt::sum(xt::square(actual - xt::mean(actual)))(0);
-
+                          xt::sum(xt::square(actual - actual_mean))(0);
     std::cout << "R2: " << r2 << std::endl;
+
+    float mape = xt::mean(xt::abs((actual - predicted) / actual))(0);
+    std::cout << "MAPE: " << mape << std::endl;
 
     std::ofstream f1("pred.csv");
     xt::dump_csv(f1, xt::expand_dims(pred, 1));
